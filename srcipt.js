bc670e8fc4750aa41a2f2a935524d3c7000990f8
Raaz <21ketanraaz@gmail.com>
2023-04-21 19:02:39 +0530

    +Catch Phrase

--- srcipt.js
+++ srcipt.js
@@ -1,9 +1,9 @@
 const text = document.querySelector('#name');
-const container = document.querySelector('.container');
+// const container = document.querySelector('.container');
 
 window.addEventListener('scroll', () => {
   const current = window.scrollY;
-  const limit = 400;
+  const limit = 500;
 
   if (current > limit) {
     return;
@@ -12,8 +12,27 @@ window.addEventListener('scroll', () => {
   text.style.fontSize = `clamp(3rem, ${current}px, 30rem)`;
   text.style.opacity = 1 - current / limit;
 
-  if (text.style.opacity <= 0.01) {
+  if (text.style.opacity <= 0.2) {
     window.removeEventListener('scroll', handleScroll);
-    container.style.opacity = 1;
+    // container.style.opacity = 0;
   }
 });
+
+const moon = document.querySelector('.moon');
+
+function changeMoon() {
+  if (moon.textContent === 'C') {
+    moon.textContent = '🌙'; // Replace "C" with crescent moon
+    moon.classList.add('hidden'); // Hide "C"
+  } else {
+    moon.textContent = 'C'; // Replace crescent moon with "C"
+    moon.classList.remove('hidden'); // Show "C"
+  }
+}
+
+setInterval(() => {
+  changeMoon();
+}, 1000);
+
+
+
