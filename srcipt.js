5157d56c09cf71928ee00b8e727280abb732e692
Raaz <21ketanraaz@gmail.com>
2023-10-05 15:58:52 +0530

    Media Quieries for 15.inches

--- srcipt.js
+++ srcipt.js
@@ -1,20 +1,15 @@
 const tipButtons = document.querySelectorAll('.tip-button')
 
-// Loop through all buttons (allows for multiple buttons on page)
+
 tipButtons.forEach((button) => {
   let coin = button.querySelector('.coin')
-
-  // The larger the number, the slower the animation
   coin.maxMoveLoopCount = 90
 
   button.addEventListener('click', () => {
     if (button.clicked) return
 
     button.classList.add('clicked')
-
-    // Wait to start flipping the coin because of the button tilt animation
     setTimeout(() => {
-      // Randomize the flipping speeds just for fun
       coin.sideRotationCount = Math.floor(Math.random() * 5) * 90
       coin.maxFlipAngle = (Math.floor(Math.random() * 4) + 3) * Math.PI
       button.clicked = true
@@ -34,7 +29,6 @@ tipButtons.forEach((button) => {
     coin.style.setProperty('--shine-opacity-multiplier', 0.4)
     coin.style.setProperty('--shine-bg-multiplier', '50%')
     coin.style.setProperty('opacity', 1)
-    // Delay to give the reset animation some time before you can click again
     setTimeout(() => {
       button.clicked = false
     }, 300)
@@ -44,15 +38,10 @@ tipButtons.forEach((button) => {
     coin.moveLoopCount++
     let percentageCompleted = coin.moveLoopCount / coin.maxMoveLoopCount
     coin.angle = -coin.maxFlipAngle * Math.pow((percentageCompleted - 1), 2) + coin.maxFlipAngle
-    
-    // Calculate the scale and position of the coin moving through the air
     coin.style.setProperty('--coin-y-multiplier', -11 * Math.pow(percentageCompleted * 2 - 1, 4) + 11)
     coin.style.setProperty('--coin-x-multiplier', percentageCompleted)
     coin.style.setProperty('--coin-scale-multiplier', percentageCompleted * 0.6)
     coin.style.setProperty('--coin-rotation-multiplier', percentageCompleted * coin.sideRotationCount)
-
-    // Calculate the scale and position values for the different coin faces
-    // The math uses sin/cos wave functions to similate the circular motion of 3D spin
     coin.style.setProperty('--front-scale-multiplier', Math.max(Math.cos(coin.angle), 0))
     coin.style.setProperty('--front-y-multiplier', Math.sin(coin.angle))
 
@@ -64,8 +53,6 @@ tipButtons.forEach((button) => {
 
     coin.style.setProperty('--shine-opacity-multiplier', 4 * Math.sin((coin.angle + Math.PI / 2) % Math.PI) - 3.2)
     coin.style.setProperty('--shine-bg-multiplier', -40 * (Math.cos((coin.angle + Math.PI / 2) % Math.PI) - 0.5) + '%')
-
-    // Repeat animation loop
     if (coin.moveLoopCount < coin.maxMoveLoopCount) {
       if (coin.moveLoopCount === coin.maxMoveLoopCount - 6) button.classList.add('shrink-landing')
       window.requestAnimationFrame(flipCoinLoop)
